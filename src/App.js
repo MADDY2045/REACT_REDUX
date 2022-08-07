68849c898f89c9c8139312836e65b37441a50a64
MADDY2045 <madhavaneee08@gmail.com>
2022-08-07 13:09:20 +0530

    use Fetch custom hook example

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
-import React, { useState, useMemo } from "react";
+import React from "react";
+import useFetch from "./useFetch";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
-
-  /* Function without useMemo will rerender even when todo function is called */
-  // const calculation = expensiveCalculation(count);
-
-  const calculation = useMemo(() => {
-    return expensiveCalculation(count);
-  }, [count]);
-
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
-  const addTodo = () => {
-    setTodos((t) => [...t, "New Todo"]);
-  };
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
 
   return (
-    <div>
-      <div>
-        <h2>My Todos</h2>
-        {todos.map((todo, index) => {
-          return <p key={index}>{todo}</p>;
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
         })}
-        <button className="btn btn-primary" onClick={addTodo}>
-          Add Todo
-        </button>
-      </div>
-      <hr />
-      <div>
-        Count: {count}
-        <button className="btn btn-warning" onClick={increment}>
-          +
-        </button>
-        <h2>Expensive Calculation</h2>
-        {calculation}
-      </div>
-    </div>
+    </>
   );
-};
-
-const expensiveCalculation = (num) => {
-  console.log("Calculating...");
-  for (let i = 0; i < 1000000000; i++) {
-    num += 1;
-  }
-  return num;
 };
 
 export default App;
